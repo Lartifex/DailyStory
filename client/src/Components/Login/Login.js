0c49300aa3571c6122af8c0dc3218aeb7d7587d5
Elena Gonzalez <egonzalezcm08@gmail.com>
2023-12-15 13:47:52 +0100

    feat: add username to cookies

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { login } from '../../services/auth';
+import toast from 'react-hot-toast';
 import Cookies from 'js-cookie';
 import './Login.css';
 
@@ -12,13 +13,18 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await login({ email, password });
+
     if (response.success && response.sessionToken) {
       Cookies.set('sessionToken', response.sessionToken, { expires: 1 });
+      Cookies.set('username', response.username, { expires: 1 });
       navigate('/');
     } else {
       console.error('Login failed:', response.message);
     }
+
+    toast.success('Welcome back!');
   };
+
   return (
     <div className="auth-container">
       <div className="auth-box">
