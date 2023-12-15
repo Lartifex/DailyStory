2fa36f575d20ece484bddf860b48d12563680d8f
Elena Gonzalez <egonzalezcm08@gmail.com>
2023-12-15 14:00:53 +0100

    feat: use username in HomePage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,21 @@
-import Footer from "../components/Footer";
-import Stories from "../components/Stories";
+import { useEffect, useState } from 'react';
+import Footer from '../components/Footer';
+import Stories from '../components/Stories';
+import Cookies from 'js-cookie';
 
 const HomePage = ({ stories }) => {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const usernameFromCookie = Cookies.get('username');
+    if (usernameFromCookie) {
+      setUsername(usernameFromCookie);
+    }
+  }, []);
+
   return (
     <div className="containerWithFooter">
-      <h1>Hi, Emma!</h1>
+      <h1>Hi, {username || '🌎'}!</h1>
       <h2>Choose your daily story:</h2>
       <div className="storiesScroll">
         <Stories stories={stories} />
